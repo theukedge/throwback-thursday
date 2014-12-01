ab66b8396f653af0ab1155c2332ba47534f38661
Dave Clements <dave@theukedge.com>
2014-12-01 12:18:46 -0500

    Correct action hook

--- includes/admin-page.php
+++ includes/admin-page.php
@@ -4,7 +4,7 @@ function thth_add_menu_page() {
 	add_management_page( 'Throwback Thursday', 'Throwback Thursday', 'manage_options', 'throwback-thursday', 'thth_admin_page_output' );
 }
 
-add_action( 'admin_menu', 'add_throwback_thursday_menu_page' );
+add_action( 'admin_menu', 'thth_add_menu_page' );
 
 function thth_admin_page_output() {
 	echo 'OUTPUT';
