b62bb1aee0a1edebe8bd75962446672c20a1e39f
Dave Clements <dave@theukedge.com>
2014-12-01 12:14:58 -0500

    Build admin page

---
+++ includes/admin-page.php
@@ -0,0 +1,11 @@
+<?php
+
+function thth_add_menu_page() {
+	add_management_page( 'Throwback Thursday', 'Throwback Thursday', 'manage_options', 'throwback-thursday', 'thth_admin_page_output' );
+}
+
+add_action( 'admin_menu', 'add_throwback_thursday_menu_page' );
+
+function thth_admin_page_output() {
+	echo 'OUTPUT';
+}
