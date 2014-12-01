550a824193dd32a88bd5f4d88c00998c5ec27468
Dave Clements <dave@theukedge.com>
2014-12-01 12:46:23 -0500

    Figure out when Throwback Thursday happens

--- includes/query.php
+++ includes/query.php
@@ -1,3 +1,13 @@
 <?php
 
-// custom query for finding correct posts will go here
+// when is Throwback Thursday?
+
+$todaysday = date ( 'w', current_time( 'timestamp' ) ); // find out what day WordPress says it is
+
+if( $todaysday == 4 ) {
+	$querydate = date( 'j' ); // today's date
+	$querymonth = date( 'n' ); // today's month
+} else {
+	$querydate = date( 'j', strtotime( 'Next Thursday' ) ); // next Thursday's date
+	$querymonth = date( 'n', strtotime( 'Next Thursday' ) ); // next Thursday's month
+}
